6ca8bfabfef0284b57aea3a0c486a8f3eb97e285
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-04-19 11:12:59 -0400

    Starting to think about views testing

--- solenoid/records/tests.py
+++ solenoid/records/tests.py
@@ -1,4 +1,9 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test import RequestFactory
+
+from .models import Record
+from .views import UnsentList
 
 # ------ MODELS ------
 # Do we want a unique_together enforcement? If so, which?
@@ -12,13 +17,40 @@ from django.test import TestCase
 # Do I need to enforce choices on the method of acq? I definitely need to clean it.
 # The guidance field has HTML formatting - how do I want to handle that? If I
 # save it as text and parse it later as html, will that work?
+# Sorts by DLC and then last name
 
 # ------ VIEWS ------
-# There is a view that lists all unsent records
 # People can filter it by DLC or search by author? Something useful, maybe with
 # autocomplete, if that's not too many yaks
-# But the first case should have it sorted by DLC and then author
+# But the first case should have it sorted by DLC and then author (per model default)
 # People can select desired records
 # (Also there should be a 'select entire DLC' option but that's JS I won't test here)
 # Posting a set of records causes the email constructor to be invoked with that set
 # There's a view that lists -invalid- records
+
+class RecordsViewsTest(TestCase):
+
+    def setup_view(view, request, *args, **kwargs):
+        """Mimic as_view() returned callable, but returns view instance.
+
+        args and kwargs are the same you would pass to ``reverse()``"""
+        view.request = request
+        view.args = args
+        view.kwargs = kwargs
+        return view
+
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.url_unsent = reverse('records:unsent_list')
+
+
+    def test_unsent_records_url_exists(self):
+        found = resolve(self.url_unsent)
+
+
+    def test_unsent_records_page_lists_all_unsent(self):
+        request = self.factory.get(self.url_unsent)
+        view = self.setup_view(UnsentList, request)
+        self.assertQuerysetEqual(view.get_queryset(),
+            Record.objects.filter(status=Record.UNSENT)
