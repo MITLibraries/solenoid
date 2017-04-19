af2101f4b725942190fba26f07b6d52e8d4f8982
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-04-19 16:42:30 -0400

    Adds a (non-working) select form to the unsent records page
    Eventually, this will let people submit one or more papers to the
    email-building functionality, and will also let them filter by DLC.
    Or maybe select/unselect all by DLC. Whichever. Right now it just
    looks like it does, getting your hopes up and then quietly crushing
    them.

--- solenoid/records/tests.py
+++ solenoid/records/tests.py
@@ -5,18 +5,9 @@ from .models import Record
 from .views import UnsentList, InvalidList
 
 # ------ MODELS ------
-# Do we want a unique_together enforcement? If so, which?
 # Do I want to enforce choices on Record.dlc?
-# A citation constructor - you're going to need one - only allow one format for
-# now - do you need to allow for multiple item types?
-#   Yes you do! You really want to use the Citation field rather than rolling
-#   your own, but it needs to be complete.
 # Error handling - what do we do when a record does not have the required data?
 # For instance, blank DLC
-# Do I need to enforce choices on the method of acq? I definitely need to clean it.
-# The guidance field has HTML formatting - how do I want to handle that? If I
-# save it as text and parse it later as html, will that work?
-# Sorts by DLC and then last name
 
 # ------ VIEWS ------
 # People can filter it by DLC or search by author? Something useful, maybe with
@@ -25,7 +16,6 @@ from .views import UnsentList, InvalidList
 # People can select desired records
 # (Also there should be a 'select entire DLC' option but that's JS I won't test here)
 # Posting a set of records causes the email constructor to be invoked with that set
-# There's a view that lists -invalid- records
 
 
 class RecordModelTest(TestCase):
@@ -115,11 +105,17 @@ class UnsentRecordsViewsTest(TestCase):
         with self.assertTemplateUsed('records/record_list.html'):
             c.get(self.url)
 
-    def test_unsent_records_page_lists_all_unsent(self):
+    def test_unsent_records_page_has_all_unsent_in_context(self):
         # assertQuerysetEqual never works, so we're just comparing the pks.
         self.assertEqual(
             set(UnsentList().get_queryset().values_list('pk')),
             set(Record.objects.filter(status=Record.UNSENT).values_list('pk')))
+
+    def test_unsent_records_page_displays_all_unsent(self):
+        c = Client()
+        response = c.get(self.url)
+        for record in Record.objects.filter(status=Record.UNSENT):
+            self.assertContains(response, record.citation)
 
 
 class InvalidRecordsViewsTest(TestCase):
--- solenoid/records/views.py
+++ solenoid/records/views.py
@@ -8,10 +8,23 @@ logger = logging.getLogger(__name__)
 
 
 class UnsentList(ListView):
+    def get_context_data(self, **kwargs):
+        context = super(UnsentList, self).get_context_data(**kwargs)
+        context['page_title'] = 'Unsent Records'
+        context['extension_template'] = 'records/_unsent_list.html'
+        context['dlcs'] = self.get_queryset().values_list('dlc', flat=True)
+        return context
+
     def get_queryset(self):
         return Record.objects.filter(status=Record.UNSENT)
 
 
 class InvalidList(ListView):
+    def get_context_data(self, **kwargs):
+        context = super(UnsentList, self).get_context_data(**kwargs)
+        context['page_title'] = 'Invalid Records'
+        context['extension_template'] = 'records/_invalid_list.html'
+        return context
+
     def get_queryset(self):
         return Record.objects.filter(status=Record.INVALID)
