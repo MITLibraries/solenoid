c1d41eaa0d5ba0522bfefb24fe76480b3f003828
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-04-19 11:12:48 -0400

    Update model
    Having seen a few example CSV files and discussed the options, I think
    this is the set of information we'll need to track/save.

--- solenoid/records/models.py
+++ solenoid/records/models.py
@@ -1,4 +1,11 @@
+from datetime import date
+import logging
+
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import pre_save
+
+logger = logging.getLogger(__name__)
 
 class Record(models.Model):
     """The Record contains:
@@ -9,11 +16,20 @@ class Record(models.Model):
           already solicited the author for this citation)."""
 
     class Meta:
-        ordering = ['-name']
+        ordering = ['dlc', 'last_name']
 
     ACQ_METHODS = (
         (0, 'RECRUIT_FROM_AUTHOR_MANUSCRIPT')
         #(1, FPV)
+    )
+
+    UNSENT = 'Unsent'
+    SENT = 'Sent'
+    INVALID = 'Invalid'
+    STATUS_CHOICES = (
+        (0, UNSENT),
+        (1, SENT),
+        (2, INVALID),
     )
 
     dlc = models.CharField(max_length=100)
@@ -23,7 +39,11 @@ class Record(models.Model):
     publisher_name = models.CharField(max_length=50)
     acq_method = models.IntegerField(choices=ACQ_METHODS)
     citation = models.TextField()
-    guidance = models.TextField(blank=True)
+    status = models.IntegerField(default=0, choices=STATUS_CHOICES)
+    status_timestamp = models.DateField(default=date.today)
+    paper_id = models.CharField(max_length=10, help_text="This is the " \
+        "Publication ID field from Elements; it is supposed to be unique but " \
+        "we will not be relying on it as a primary key here.")
 
     ## I don't need ANY of these if I can get full citation.
     #title = models.TextField()
@@ -33,3 +53,25 @@ class Record(models.Model):
     #volume = models.CharField(max_length=6, blank=True, null=True)
     #issue = models.CharField(max_length=3, blank=True, null=True)
     #year_published = models.DateField()
+
+
+@receiver(pre_save, sender=Record)
+def verify_status(sender, instance, **kwargs):
+    """Make sure that any status changes are valid. If so, update the
+    status_timestamp."""
+    # There are (from, to) tuples representing invalid status transitions
+    # (e.g. you may not go from SENT to UNSENT).
+    invalid_changes = ((1, 0), (1, 2))
+
+    try:
+        original = Record.objects.get(pk=instance.pk)
+    except Record.DoesNotExist:
+        pass
+    else:
+        if not original.status == instance.status:
+            if (original.status, instance.status) in invalid_changes:
+                logger.warning("Attempt make an invalid status change from " \
+                    "{x} to {y}".format(x=original.status, y=instance.status))
+                raise ValidationError("That status change is not allowed.")
+            else:
+                instance.status_timestamp = date.today()
