8f76201dee1a5b9efe2b7bad7128d4ead9f592fe
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-04-20 13:28:17 -0400

    Make tests pass again following refactor

--- solenoid/records/views.py
+++ solenoid/records/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.views.generic.list import ListView
 
+from solenoid.people.models import Author
 from .models import Record
 
 logger = logging.getLogger(__name__)
@@ -12,7 +13,7 @@ class UnsentList(ListView):
         context = super(UnsentList, self).get_context_data(**kwargs)
         context['page_title'] = 'Unsent Records'
         context['extension_template'] = 'records/_unsent_list.html'
-        context['dlcs'] = self.get_queryset().values_list('dlc', flat=True)
+        context['dlcs'] = Author.objects.filter(record__in=self.get_queryset()).values_list('dlc', flat=True)
         return context
 
     def get_queryset(self):
@@ -21,7 +22,7 @@ class UnsentList(ListView):
 
 class InvalidList(ListView):
     def get_context_data(self, **kwargs):
-        context = super(UnsentList, self).get_context_data(**kwargs)
+        context = super(InvalidList, self).get_context_data(**kwargs)
         context['page_title'] = 'Invalid Records'
         context['extension_template'] = 'records/_invalid_list.html'
         return context
