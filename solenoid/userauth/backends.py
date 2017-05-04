afbd6dd011670a5d920777faf6a6a84143bc8a61
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-05-04 14:14:54 -0400

    Authenticate with MIT
    This makes the token exchange parts work. It doesn't work end-to-end
    for the user yet because it doesn't yet know how to harvest data from
    the response.

--- solenoid/userauth/backends.py
+++ solenoid/userauth/backends.py
@@ -1,4 +1,5 @@
 from social_core.backends.oauth import BaseOAuth2
+from social_core.utils import url_add_parameters
 
 
 class MITOAuth2(BaseOAuth2):
@@ -11,3 +12,18 @@ class MITOAuth2(BaseOAuth2):
         """Return user details from MIT account"""
         return {'name': response.get('name'),
                 'email': response.get('email') or ''}
+
+    def auth_complete_credentials(self):
+        return self.get_key_and_secret()
+
+    def access_token_url(self):
+        base_url = super(MITOAuth2, self).access_token_url()
+        params = {
+            'grant_type': 'authorization_code',
+            'code': self.data['code'],
+            'redirect_uri': self.get_redirect_uri()
+        }
+        return url_add_parameters(base_url, params)
+
+    def get_redirect_uri(self, state=None):
+        return self.redirect_uri
