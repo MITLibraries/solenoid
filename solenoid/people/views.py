0dd70ad728935c1cc4c764dee19685e54f99f989
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-05-17 16:26:12 -0400

    Now liaisons can be edited again

--- solenoid/people/views.py
+++ solenoid/people/views.py
@@ -3,13 +3,12 @@ import logging
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
-from django.views.generic.base import View
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
 from solenoid.userauth.mixins import LoginRequiredMixin
 
-from .forms import LiaisonCreateForm
+from .forms import LiaisonCreateForm, LiaisonDLCForm
 from .models import Liaison, DLC
 
 logger = logging.getLogger(__name__)
@@ -19,6 +18,17 @@ class LiaisonCreate(LoginRequiredMixin, CreateView):
     model = Liaison
     form_class = LiaisonCreateForm
     success_url = reverse_lazy('people:liaison_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(LiaisonCreate, self).get_context_data(**kwargs)
+        context['title'] = 'Add liaison'
+        context['breadcrumbs'] = [
+            {'url': reverse_lazy('home'), 'text': 'dashboard'},
+            {'url': reverse_lazy('people:liaison_list'),
+                'text': 'manage liaisons'},
+            {'url': '#', 'text': 'edit liaison'}
+        ]
+        return context
 
     def form_valid(self, form):
         liaison = form.save()
@@ -32,26 +42,51 @@ class LiaisonList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(LiaisonList, self).get_context_data(**kwargs)
         context['dlc_set'] = DLC.objects.all()
+        context['title'] = 'Manage liaisons'
+        context['breadcrumbs'] = [
+            {'url': reverse_lazy('home'), 'text': 'dashboard'},
+            {'url': '#', 'text': 'manage liaisons'}
+        ]
         return context
 
 
-class LiaisonUpdate(LoginRequiredMixin, View):
+class LiaisonUpdate(LoginRequiredMixin, UpdateView):
+    model = Liaison
+    fields = ('first_name', 'last_name', 'email_address')
+    success_url = reverse_lazy('people:liaison_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(LiaisonUpdate, self).get_context_data(**kwargs)
+        context['title'] = 'Edit liaison'
+        context['breadcrumbs'] = [
+            {'url': reverse_lazy('home'), 'text': 'dashboard'},
+            {'url': reverse_lazy('people:liaison_list'),
+                'text': 'manage liaisons'},
+            {'url': '#', 'text': 'edit liaison'}
+        ]
+        context['dlc_form'] = LiaisonDLCForm()
+        return context
+
     def post(self, request, *args, **kwargs):
-        try:
-            dlcs = DLC.objects.filter(pk__in=request.POST.getlist('dlc'))
-        except KeyError:
-            logger.exception()
-            raise
+        # This would normally be set in post(), but we're not calling super, so
+        # we need to do it ourselves.
+        self.object = self.get_object()
 
-        try:
-            liaison = Liaison.objects.get(pk=self.kwargs['pk'])
-        except (KeyError, Liaison.DoesNotExist):
-            logger.exception()
-            raise
+        form = self.get_form()
 
-        liaison.dlc_set.clear()
-        liaison.dlc_set.add(*dlcs)
-        messages.success(request, 'DLCs updated for {l.first_name} '
-            '{l.last_name}.'.format(l=liaison))
+        if form.is_valid():
+            try:
+                dlcs = DLC.objects.filter(pk__in=request.POST.getlist('dlc'))
+            except KeyError:
+                logger.exception()
+                raise
 
-        return HttpResponseRedirect(reverse_lazy('people:liaison_list'))
+            liaison = self.get_object()
+            liaison.dlc_set.clear()
+            liaison.dlc_set.add(*dlcs)
+            messages.success(request, 'Liaison updated.')
+            return self.form_valid(form)
+
+        else:
+            messages.warning(request, 'Please correct the errors below.')
+            return self.form_invalid(form)
