5fbdd2c4f4444d2bd544d4c65ce910591086e7b7
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-05-19 09:44:53 -0400

    Add people.admin for ease of administration

--- solenoid/people/admin.py
+++ solenoid/people/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Liaison, DLC, Author
+
+
+class DLCInline(admin.TabularInline):
+    model = DLC
+    fields = ('name',)
+
+
+class LiaisonAdmin(admin.ModelAdmin):
+    list_filter = ('active',)
+    list_display = ('first_name', 'last_name', 'email_address', 'active')
+    inlines = (DLCInline,)
+
+
+admin.site.register(Liaison, LiaisonAdmin)
+
+
+class DLCAdmin(admin.ModelAdmin):
+    list_filter = ('liaison',)
+    list_display = ('name', 'liaison')
+
+
+admin.site.register(DLC, DLCAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('dlc', 'dlc__liaison')
+    list_display = ('first_name', 'last_name', 'email', 'dlc',)
+
+
+admin.site.register(Author, AuthorAdmin)
