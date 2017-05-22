6fbfc716911f4d7f11b419bcd5170705048415b4
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-05-22 16:55:09 -0400

    Allow users to deselect all DLCs without error

--- solenoid/people/forms.py
+++ solenoid/people/forms.py
@@ -11,7 +11,8 @@ class LiaisonCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(LiaisonCreateForm, self).__init__(*args, **kwargs)
         self.fields['dlc'] = forms.ModelMultipleChoiceField(
-            queryset=DLC.objects.all())
+            queryset=DLC.objects.all(),
+            required=False)  # allow 0 DLCs to be selected
         self.fields['dlc'].widget.attrs['class'] = 'field field-select'
         self.fields['first_name'].widget.attrs['class'] = 'field field-text'
         self.fields['last_name'].widget.attrs['class'] = 'field field-text'
