23f657efa6f273cbb1fd1b5d3f3fe5cfde560769
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-05-22 12:25:52 -0400

    Make dashboard behavior sensitive to whether liaisons already exist
    If there *aren't* any liaisons, 'create' is the needed behavior. If
    there are, 'manage' is more useful (they can always create new ones
    from that page).

--- solenoid/views.py
+++ solenoid/views.py
@@ -1,7 +1,20 @@
+from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
 
+from solenoid.people.models import Liaison
 from solenoid.userauth.mixins import LoginRequiredMixin
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        if Liaison.objects.count():
+            context['liaison_url'] = reverse('people:liaison_list')
+            context['liaison_text'] = 'manage'
+        else:
+            context['liaison_url'] = reverse('people:liaison_create')
+            context['liaison_text'] = 'create'
+
+        return context
