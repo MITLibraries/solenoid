1071915e50f4806f91876b6dfc1613172aafe4da
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-07-21 17:17:17 -0400

    Update required CSV
    We expect the CSV to have headers for various journal metadata (we'll
    index into the dict using these keys). However, we don't expect them
    all to be nonempty.
    
    We DO need source and record ID to be nonempty or we can't use the
    Elements API.

--- solenoid/records/helpers.py
+++ solenoid/records/helpers.py
@@ -12,10 +12,17 @@ class Headers(object):
     MESSAGE = 'c-publisher-related-email-message'
     SOURCE = 'Data Source (Publication)'
     RECORD_ID = 'Data Source Proprietary ID (Publication)'
+    # Note - this seems to be a string like '20050601' and not an actual year,
+    # column title notwithstanding.
+    PUBDATE = 'Year Published'
+    TITLE = 'Title1'
+    JOURNAL = 'Journal'
+    VOLUME = 'Volume'
+    ISSUE = 'Issue'
 
     EXPECTED_HEADERS = [EMAIL, DOI, FIRST_NAME, LAST_NAME, MIT_ID, CITATION,
                         PUBLISHER_NAME, ACQ_METHOD, DLC, PAPER_ID, MESSAGE,
-                        SOURCE, RECORD_ID]
+                        SOURCE, RECORD_ID, TITLE, JOURNAL, VOLUME, ISSUE]
 
     # This is information we can get from the database if it happens to be
     # missing from a row in an Elements CSV file, if we already know about
@@ -23,14 +30,19 @@ class Headers(object):
     # the author.
     AUTHOR_DATA = [EMAIL, FIRST_NAME, LAST_NAME, DLC]
 
+    # This is the data we need to construct a minimal citation, if the citation
+    # field is blank.
+    CITATION_DATA = [FIRST_NAME, LAST_NAME, TITLE, JOURNAL]
+
     # And this is the information from EXPECTED_HEADERS that we can't find if
     # it isn't in the CSV.
     # Some information is optional because...
     # * DOI: only needed for FPV manuscripts - the model is responsible for
     #   checking.
     # * MESSAGE: not all publishers have a special message.
-    # * SOURCE and RECORD_ID: convenient to have, but we can acquire from the
-    #   API if we must.
+    # * CITATION and citation-related data: We can construct a minimal citation
+    #   from other data; alternately, we don't need the other data if we have a
+    #   citation. The Record model will check for this.
     REQUIRED_DATA = list(set(EXPECTED_HEADERS) -
-                         set(AUTHOR_DATA) - {DOI} - {MESSAGE} -
-                         {SOURCE} - {RECORD_ID})
+                         set(AUTHOR_DATA) - {DOI} - {MESSAGE} - {CITATION}
+                         - {TITLE} - {JOURNAL} - {VOLUME} - {ISSUE})
