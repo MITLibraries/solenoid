930dcf5a9708f55d5843bffdfe000c9cc9d84751
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-08-11 10:43:38 -0400

    Missed in previous commit

---
+++ solenoid/records/migrations/0011_auto_20170811_1433.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('records', '0010_auto_20170802_1425'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='record',
+            name='publisher_name',
+            field=models.CharField(max_length=75),
+        ),
+    ]
