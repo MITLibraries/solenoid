d98bf1686102485aabf99f30f4b1f20a316e93ff
Helen Bailey <hakbailey@gmail.com>
2020-04-23 16:13:01 -0400

    Fix database migration error

--- solenoid/records/migrations/0015_auto_20200403_1856.py
+++ solenoid/records/migrations/0015_auto_20200403_1856.py
@@ -10,12 +10,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='record',
-            name='message',
-            field=models.TextField(blank=True, default=''),
-            preserve_default=False,
-        ),
+        migrations.RenameField(model_name='record',
+                               old_name='message',
+                               new_name='message_old'),
+        migrations.AddField(model_name='record',
+                            name='message',
+                            field=models.TextField(blank=True, default=''),
+                            preserve_default=False),
+        migrations.RemoveField(model_name='record',
+                               name='message_old'),
         migrations.DeleteModel(
             name='Message',
         ),
